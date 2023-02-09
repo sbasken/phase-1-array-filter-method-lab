81790ca5f0641a4b48c012a6883c340b8db53b2b
Saki <sbasken@gmail.com>
2023-02-08 17:02:25 -0800

    add three functions to index.js

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Code your solution here
+const findMatching = (array, string) => {
+  return array.filter(name => name.toLowerCase() === string.toLowerCase())
+}
+
+const fuzzyMatch = (array, string) => {
+    return array.filter(name => name[0].toUpperCase() === string[0].toUpperCase())
+}
+
+const matchName = (drivers, string) => {
+    return drivers.filter(driver => driver.name.toLowerCase() === string.toLowerCase())
+}
